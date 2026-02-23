11681474c7815298553f17a68bf95b85d5a1e0ff
Ashik Mridha <ashikmridha43@gmail.com>
2026-02-23 23:47:14 +0600

    change location name and possition

--- script/job.js
+++ script/job.js
@@ -1,12 +1,12 @@
 const jobs = [
-    { id: 1, company: "Google", position: "Frontend Dev", location: "Remote", type: "Full Time", salary: "$5000", description: "Build UI", status: "all" },
-    { id: 2, company: "Amazon", position: "Backend Dev", location: "USA", type: "Full Time", salary: "$6000", description: "Work on APIs", status: "all" },
-    { id: 3, company: "Netflix", position: "UI Designer", location: "Remote", type: "Contract", salary: "$4000", description: "Design screens", status: "all" },
-    { id: 4, company: "Meta", position: "React Dev", location: "Canada", type: "Full Time", salary: "$5500", description: "Develop apps", status: "all" },
-    { id: 5, company: "Microsoft", position: "QA Engineer", location: "UK", type: "Full Time", salary: "$4500", description: "Test software", status: "all" },
-    { id: 6, company: "Adobe", position: "UX Designer", location: "Remote", type: "Contract", salary: "$4200", description: "Improve UX", status: "all" },
-    { id: 7, company: "Tesla", position: "Fullstack Dev", location: "USA", type: "Full Time", salary: "$6500", description: "Build systems", status: "all" },
-    { id: 8, company: "Spotify", position: "Mobile Dev", location: "Remote", type: "Part Time", salary: "$3500", description: "Develop apps", status: "all" }
+    { id: 1, company: "Google", position: "Full Time Dev", location: "Rajshahi", type: "Full Time", salary: "$5000", description: "Build UI", status: "all" },
+    { id: 2, company: "Amazon", position: "Part Time Dev", location: "Chottogram", type: "Full Time", salary: "$6000", description: "Work on APIs", status: "all" },
+    { id: 3, company: "Netflix", position: "Fullstack Dev", location: "Dhaka", type: "Contract", salary: "$4000", description: "Design screens", status: "all" },
+    { id: 4, company: "Meta", position: "Backend Dev", location: "Sylate", type: "Full Time", salary: "$5500", description: "Develop apps", status: "all" },
+    { id: 5, company: "Microsoft", position: "Frontend Dev", location: "Joshor", type: "Full Time", salary: "$4500", description: "Test software", status: "all" },
+    { id: 6, company: "Adobe", position: "React Dev", location: "Dhaka", type: "Contract", salary: "$4200", description: "Improve UX", status: "all" },
+    { id: 7, company: "Tesla", position: "Fullstack Dev", location: "Kumilla", type: "Full Time", salary: "$6500", description: "Build systems", status: "all" },
+    { id: 8, company: "Spotify", position: "Mobile Dev", location: "Khulna", type: "Part Time", salary: "$3500", description: "Develop apps", status: "all" }
 ];
 
 let currentTab = "all";
